b1dae5458cb5adf3b3ad51789d266dd5dc052a69
David <david.beley1@gmail.com>
2017-12-25 20:40:51 +0100

    fish : désactivation vi mode

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,7 +1,5 @@
 source /etc/profile.d/autojump.fish
 
-fish_vi_key_bindings
-
 set -g theme_display_vi no
 set -g theme_display_date no
 set -g theme_nerd_fonts yes
--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,5 +1,5 @@
 function fish_user_key_bindings
-	bind ! bind_bang
+    bind '!' bind_bang
     bind '$' bind_dollar
 end
 
