f5de6f1c965b4492261cff6deadb193e43eafc39
dbeley <david.beley@netc.fr>
2018-05-05 00:20:22 +0200

    modifs emacs

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -32,10 +32,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("d96587ec2c7bf278269b8ec2b800c7d9af9e22d816827639b332b0e613314dfd" "196df8815910c1a3422b5f7c1f45a72edfa851f6a1d672b7b727d9551bb7c7ba" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" "d9dab332207600e49400d798ed05f38372ec32132b3f7d2ba697e59088021555" default)))
  '(inhibit-startup-screen t)
  '(package-selected-packages
    (quote
-    (esup counsel use-package ranger projectile ox-twbs ox-reveal org-plus-contrib org-bullets no-littering magit htmlize helm exwm-x evil-surround elpy company-quickhelp better-defaults base16-theme airline-themes ace-window))))
+    (telephone-line spaceline-config spaceline emms esup counsel use-package ranger projectile ox-twbs ox-reveal org-plus-contrib org-bullets no-littering magit htmlize helm exwm-x evil-surround elpy company-quickhelp better-defaults base16-theme airline-themes ace-window))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
