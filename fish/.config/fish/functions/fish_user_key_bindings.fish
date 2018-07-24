bdc6d4b0b47fdbcdbef6e3b396f88cd0ba5f4080
dbeley <david.beley@netc.fr>
2018-07-24 16:50:00 +0200

    modifs fish : ajout  bang-bang

--- fish/.config/fish/functions/fish_user_key_bindings.fish
+++ fish/.config/fish/functions/fish_user_key_bindings.fish
@@ -1,6 +1,6 @@
 function fish_user_key_bindings
-    bind '!' bind_bang
-    bind '$' bind_dollar
+    #bind '!' bind_bang
+    #bind '$' bind_dollar
     fzf_key_bindings
     for mode in insert default visual
         bind -M $mode \cf forward-char
