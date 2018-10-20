81e1d66fb737e2a92ee387cf9ec85dd9a0d1be77
dbeley <david.beley@netc.fr>
2018-10-20 11:50:19 +0200

    modifs emacs pour exwm

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -36,7 +36,7 @@
    '("3c83b3676d796422704082049fc38b6966bcad960f896669dfc21a7a37a748fa" "065efdd71e6d1502877fd5621b984cded01717930639ded0e569e1724d058af8" "6daa09c8c2c68de3ff1b83694115231faa7e650fdbb668bc76275f0f2ce2a437" "d96587ec2c7bf278269b8ec2b800c7d9af9e22d816827639b332b0e613314dfd" "196df8815910c1a3422b5f7c1f45a72edfa851f6a1d672b7b727d9551bb7c7ba" "003a9aa9e4acb50001a006cfde61a6c3012d373c4763b48ceb9d523ceba66829" "d9dab332207600e49400d798ed05f38372ec32132b3f7d2ba697e59088021555" default))
  '(inhibit-startup-screen t)
  '(package-selected-packages
-   '(mu4e helpful cider clojure-mode simple-mpc mingus perspective perspective-el md4rd sublimity ess-site spaceline-all-the-icons all-the-icons smart-mode-line minimap sublimity-map sublimity-scroll twittering-mode flycheck smartparens emms esup counsel use-package ranger projectile ox-twbs ox-reveal org-plus-contrib org-bullets no-littering magit htmlize helm exwm-x evil-surround elpy company-quickhelp better-defaults base16-theme ace-window)))
+   '(minions moody mu4e helpful cider clojure-mode simple-mpc mingus perspective perspective-el md4rd sublimity ess-site spaceline-all-the-icons all-the-icons smart-mode-line minimap sublimity-map sublimity-scroll twittering-mode flycheck smartparens emms esup counsel use-package ranger projectile ox-twbs ox-reveal org-plus-contrib org-bullets no-littering magit htmlize helm exwm-x evil-surround elpy company-quickhelp better-defaults base16-theme ace-window)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
