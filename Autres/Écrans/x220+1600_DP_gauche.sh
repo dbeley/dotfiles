65d8620bac2c77e8b1edfcd4b73bfe89176a3f75
dbeley <david.beley@netc.fr>
2018-11-09 19:06:35 +0100

    modifs profils d'écrans

---
+++ Autres/Écrans/x220+1600_DP_gauche.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --off --output LVDS-1 --primary --mode 1366x768 --pos 1600x132 --rotate normal --output HDMI-3 --off --output HDMI-2 --off --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --mode 1600x900 --pos 0x0 --rotate normal
