f2158b2cccd6a4918a2187b4b1971ee83c5035b5
dbeley <6568955+dbeley@users.noreply.github.com>
2019-04-20 00:09:01 +0200

    ajout profil écran

---
+++ Autres/Écrans/VGA_1600x900_gauche+DP_1600x900_droite_vertical.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output VGA-1 --mode 1600x900 --pos 0x0 --rotate normal --output LVDS-1 --off --output HDMI-3 --off --output HDMI-2 --primary --mode 1600x900 --pos 1600x0 --rotate left --output HDMI-1 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off
