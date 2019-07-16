8aa2b1a126c1d44d7c4ed9c9da40462458556382
dbeley <6568955+dbeley@users.noreply.github.com>
2019-07-16 19:07:25 +0200

    Update xmonad.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -35,16 +35,15 @@ main = do
         , focusedBorderColor = "#888888"
         }
         `additionalKeys`
-        [ ((myModMask, xK_p), spawn "rofi -show run")
-        -- , ((myModMask, xK_d), spawn "rofi -show run")
-        -- , ((0, xK_Print), spawn 'sleep 0.2; maim ~/Nextcloud/Images/Captures\ d\'écran/$(date +%s).png')
-        -- , ((shiftMask, xK_Print), spawn "sleep 0.2; maim -s -o ~/Nextcloud/Images/Captures\ d\'écran/$(date +%s)_cropped.png")
+        [ ((myModMask, xK_d), spawn "rofi -show run")
+        , ((myModMask, xK_p), spawn "~/scripts/powermenu.sh")
+        , ((0, xK_Print), spawn "maim ~/Nextcloud/Images/Screenshots/$(date +%s).png")
+        , ((shiftMask, xK_Print), spawn "maim -s -o ~/Nextcloud/Images/Screenshots/$(date +%s)_cropped.png")
         , ((0, xF86XK_ScreenSaver), spawn "slock")
         , ((0, xF86XK_Suspend), spawn "slock")
         , ((0, xF86XK_TouchpadToggle), spawn "~/scripts/touchpad_toggle.sh")
         , ((0, xK_Scroll_Lock), spawn "~/scripts/change_layout.sh")
         , ((0, xF86XK_Display), spawn "~/scripts/display_config.sh")
-        , ((shiftMask, xK_F1), spawn "~/scripts/buku.sh")
         , ((0, xF86XK_Launch1), spawn "~/scripts/search.sh")
         , ((shiftMask, xF86XK_Launch1), spawn "~/scripts/launch.sh")
         , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -q sset Master 1%+")
@@ -53,9 +52,10 @@ main = do
         , ((shiftMask, xF86XK_AudioLowerVolume), spawn "amixer -q sset Master 10%-")
         , ((0, xF86XK_AudioMute), spawn "amixer -q sset Master toggle")
         , ((myModMask, xK_backslash), withFocused hideWindow)
-        -- , ((myModMask, xK_F9), scratchpadSpawnActionTerminal myTerminal)
+        -- Named scratchpads
         , ((myModMask, xK_F9), namedScratchpadAction scratchpads "keepassxc")
         , ((myModMask, xK_F10), namedScratchpadAction scratchpads "nextcloud")
+        , ((myModMask, xK_F11), namedScratchpadAction scratchpads "gnome-system-monitor")
         ]
 
 
@@ -69,5 +69,6 @@ myManageHook = composeAll
 scratchpads = [
     -- fullscreen scratchpads
     NS "keepassxc" "keepassxc" (className =? "keepassxc") (customFloating $ W.RationalRect 0 0 1 1),
-    NS "nextcloud" "nextcloud" (className =? "Nextcloud") (customFloating $ W.RationalRect 0 0 1 1)
+    NS "nextcloud" "nextcloud" (className =? "Nextcloud") (customFloating $ W.RationalRect 0 0 1 1),
+    NS "gnome-system-monitor" "gnome-system-monitor" (className =? "gnome-system-monitor") (customFloating $ W.RationalRect 0 0 1 1)
     		  ] where role = stringProperty "WM_WINDOW_ROLE"
