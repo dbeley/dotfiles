0ab5b2954089625035e71cf91de1e347f651a9dc
dbeley <6568955+dbeley@users.noreply.github.com>
2019-07-22 13:12:10 +0200

    Update xmonad, xmobar.

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -29,87 +29,167 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.ThreeColumns
 import XMonad.ManageHook
 import XMonad.StackSet as W
-import XMonad.Util.EZConfig(additionalKeys)
+import XMonad.Util.EZConfig(additionalKeysP)
 import XMonad.Util.Loggers
 import XMonad.Util.NamedScratchpad
 import XMonad.Util.Run(spawnPipe)
+import XMonad.Util.SpawnOnce
+-- import XMonad.Actions.Navigation2D
+import XMonad.Actions.DynamicProjects
+import XMonad.Actions.SpawnOn
+import XMonad.Layout.MultiToggle
+import XMonad.Layout.MultiToggle.Instances
+import XMonad.Prompt.XMonad
+import XMonad.Prompt
 
 main = do
     xmproc <- spawnPipe "xmobar"
-    xmonad $ ewmh $ defaultConfig
-    	-- { workspaces = ["1:web","2:term","3:steam","4:dev","5:office","6","7","8","9","0","-","="]
-        { manageHook = manageDocks <+> manageHook defaultConfig <+> myManageHook <+> namedScratchpadManageHook scratchpads
-        , layoutHook = avoidStruts $ spacingRaw True (Border 10 10 10 10) True (Border 4 4 4 4) True $ 
-                       layoutHook defaultConfig ||| 
-                       noBorders Full |||
-                       ThreeColMid 1 (3/100) (1/2) |||
-                       Grid
-        , handleEventHook = handleEventHook defaultConfig <+> docksEventHook
-        , logHook = dynamicLogWithPP xmobarPP
-            { ppOutput    = hPutStrLn xmproc
-            , ppTitle     = xmobarColor "gray" "" . shorten 50
-            , ppCurrent   = xmobarColor "white" ""
-            , ppVisible   = xmobarColor "white" "" . wrap "<" ">"
-            , ppWsSep     = " | "
-            , ppSort      = fmap (namedScratchpadFilterOutWorkspace.) (ppSort xmobarPP)
-            , ppLayout    = const ""
-            -- , ppExtras = [ padL battery ]
-            }
-        , terminal = myTerminal
-        , modMask = myModMask
-        , borderWidth = myBorderWidth
-        , normalBorderColor = "#444444"
-        , focusedBorderColor = "#888888"
-        }
-        `additionalKeys`
-        [ 
-        -- Launch Program
-        ((myModMask, xK_d), spawn "rofi -show run")
-        -- User Scripts
-        , ((myModMask, xK_p), spawn "~/scripts/powermenu.sh")
-        , ((0, xF86XK_TouchpadToggle), spawn "~/scripts/touchpad_toggle.sh")
-        , ((0, xK_Scroll_Lock), spawn "~/scripts/change_layout.sh")
-        , ((0, xF86XK_Display), spawn "~/scripts/display_config.sh")
-        , ((0, xF86XK_Launch1), spawn "~/scripts/search.sh")
-        , ((shiftMask, xF86XK_Launch1), spawn "~/scripts/launch.sh")
-        -- Screenshot
-        , ((0, xK_Print), spawn "maim ~/Nextcloud/Images/Screenshots/$(date +%s).png")
-        , ((shiftMask, xK_Print), spawn "maim -s -o ~/Nextcloud/Images/Screenshots/$(date +%s)_cropped.png")
-        -- Lock
-        , ((0, xF86XK_ScreenSaver), spawn "slock")
-        , ((0, xF86XK_Suspend), spawn "slock")
-        -- Volume
-        , ((0, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse -q sset Master 1%+")
-        , ((shiftMask, xF86XK_AudioRaiseVolume), spawn "amixer -D pulse -q sset Master 10%+")
-        , ((0, xF86XK_AudioLowerVolume), spawn "amixer -D pulse -q sset Master 1%-")
-        , ((shiftMask, xF86XK_AudioLowerVolume), spawn "amixer -D pulse -q sset Master 10%-")
-        , ((0, xF86XK_AudioMute), spawn "amixer -D pulse -q sset Master toggle")
-        -- Named Scratchpads
-        , ((myModMask, xK_F9), namedScratchpadAction scratchpads "keepassxc")
-        , ((myModMask, xK_F10), namedScratchpadAction scratchpads "nextcloud")
-        , ((myModMask, xK_F11), namedScratchpadAction scratchpads "gnome-system-monitor")
-        -- Workspace/Window Navigation
-        , ((0, xF86XK_Back) , moveTo Prev NonEmptyWS)
-        , ((0, xF86XK_Forward) , moveTo Next NonEmptyWS)
-		, ((myModMask, xK_g), goToSelected defaultGSConfig)
-        -- Persistent Window
-        , ((myModMask, xK_v ), windows copyToAll) -- @@ Make focused window always visible
-        , ((myModMask .|. shiftMask, xK_v ),  killAllOtherCopies) -- @@ Toggle window state back
-        -- Toggle Xmobar
-		, ((0, xK_Pause), sendMessage ToggleStruts)
-        ]
+    xmonad
+    	$ dynamicProjects projects
+    	$ ewmh
+        -- $ navigation2D def
+        -- 	(xK_Up, xK_Left, xK_Down, xK_Right)
+        -- 	[(mod4Mask,               windowGo  ),
+        --  	 (mod4Mask .|. shiftMask, windowSwap)]
+        -- 	False
+        $ defaultConfig
+        	{ manageHook = myManageHook
+         -- , { workspaces = ["1:web","2:term","3:steam","4:dev","5:office","6","7","8","9","0","-","="]
+        	, startupHook = myStartupHook
+         	, layoutHook = myLayoutHook
+         	, handleEventHook = myHandleEventHook
+         	, logHook = dynamicLogWithPP xmobarPP
+  	       	   { ppOutput    = hPutStrLn xmproc
+  	       	   , ppTitle     = xmobarColor "gray" "" . shorten 50
+  	       	   , ppCurrent   = xmobarColor "white" ""
+  	       	   , ppVisible   = xmobarColor "white" "" . wrap "<" ">"
+  	       	   , ppWsSep     = " | "
+  	       	   , ppSort      = fmap (namedScratchpadFilterOutWorkspace.) (ppSort xmobarPP)
+  	       	   , ppLayout    = const ""
+  	       	   -- , ppExtras = [ padL battery ]
+  	       	   }
+         	, terminal = myTerminal
+         	, modMask = myModMask
+         	, borderWidth = myBorderWidth
+         	, normalBorderColor = "#444444"
+         	, focusedBorderColor = "#888888"
+         	} `additionalKeysP` myKeys
 
 myTerminal = "termite"
-myModMask = mod4Mask
-myBorderWidth = 3
-myManageHook = composeAll
-    [
-    className =? "Gimp" --> doFloat
-    ]
 
-scratchpads = [
+myModMask = mod4Mask
+
+myBorderWidth = 3
+
+myStartupHook = do
+	spawnOnce "compton --config ~/.config/compton.conf"
+
+myScratchpads = [
     -- fullscreen scratchpads
     NS "keepassxc" "keepassxc" (className =? "keepassxc") (customFloating $ W.RationalRect 0 0 1 1),
     NS "nextcloud" "nextcloud" (className =? "Nextcloud") (customFloating $ W.RationalRect 0 0 1 1),
     NS "gnome-system-monitor" "gnome-system-monitor" (className =? "Gnome-system-monitor") (customFloating $ W.RationalRect 0 0 1 1)
 			  ] where role = stringProperty "WM_WINDOW_ROLE"
+
+myManageHook = manageDocks <+> 
+			   manageHook defaultConfig <+> 
+			   composeAll
+    	       [
+    	       className =? "Gimp" --> doFloat
+    	       ] <+>
+    		   namedScratchpadManageHook myScratchpads 
+
+myLayoutHook = avoidStruts
+             . spacingRaw True (Border 10 10 10 10) True (Border 4 4 4 4) True
+             . mkToggle (NBFULL ?? EOT)
+             -- $ tiled ||| Mirror tiled
+             -- $ Grid ||| noBorders Full
+             $ layoutHook defaultConfig
+             -- $ ThreeColMid 1 (3/100) (1/2)
+
+myHandleEventHook = handleEventHook defaultConfig <+> docksEventHook
+
+-- Workspaces
+
+wsSYS = "SYS"
+wsWEB = "WEB"
+
+projects :: [Project]
+projects =
+    [ Project { projectName	= wsSYS
+           , projectDirectory = "~/"
+           , projectStartHook = Just $ do spawnOn wsSYS myTerminal
+                                          spawnOn wsSYS myTerminal
+                                          spawnOn wsSYS myTerminal
+           }
+	, Project { projectName = wsWEB
+              , projectDirectory = "~/"
+              , projectStartHook = Just $ do spawn "qutebrowser"
+              }
+ ]
+
+myKeys = 
+		[ 
+        -- Launch Program
+        ("M-d", spawn "rofi -show run")
+	    , ("M-<Return>", spawn myTerminal)
+        -- User Scripts
+        , ("M-p", spawn "~/scripts/powermenu.sh")
+        , ("<XF86TouchpadToggle>", spawn "~/scripts/touchpad_toggle.sh")
+        , ("<Scroll_Lock>", spawn "~/scripts/change_layout.sh")
+        , ("<XF86Display>", spawn "~/scripts/display_config.sh")
+        , ("<XF86Launch1>", spawn "~/scripts/search.sh")
+        , ("S-<XF86Launch1>", spawn "~/scripts/launch.sh")
+        -- Screenshot
+        , ("<Print>", spawn "maim ~/Nextcloud/Images/Screenshots/$(date +%s).png")
+        , ("S-<Print>", spawn "maim -s -o ~/Nextcloud/Images/Screenshots/$(date +%s)_cropped.png")
+        -- Lock
+        , ("<XF86ScreenSaver>", spawn "slock")
+        , ("<XF86Suspend>", spawn "slock")
+        -- Volume
+        , ("<XF86AudioRaiseVolume>", spawn "amixer -D pulse -q sset Master 1%+")
+        , ("S-<XF86AudioRaiseVolume>", spawn "amixer -D pulse -q sset Master 10%+")
+        , ("<XF86AudioLowerVolume>", spawn "amixer -D pulse -q sset Master 1%-")
+        , ("S-<XF86AudioLowerVolume>", spawn "amixer -D pulse -q sset Master 10%-")
+        , ("<XF86AudioMute>", spawn "amixer -D pulse -q sset Master toggle")
+        -- Named Scratchpads
+        , ("M-<F9>", namedScratchpadAction myScratchpads "keepassxc")
+        , ("M-<F10>", namedScratchpadAction myScratchpads "nextcloud")
+        , ("M-<F11>", namedScratchpadAction myScratchpads "gnome-system-monitor")
+        -- Workspace/Window Navigation
+        , ("<XF86Back>", moveTo Prev NonEmptyWS)
+        , ("<XF86Forward>", moveTo Next NonEmptyWS)
+        , ("M-<Escape>", moveTo Next NonEmptyWS)
+    	, ("M-S-<Escape>", moveTo Prev NonEmptyWS)
+		, ("M-g", goToSelected defaultGSConfig)
+        -- Persistent Window
+        , ("M-v", windows copyToAll) -- @@ Make focused window always visible
+        , ("M-S-v",  killAllOtherCopies) -- @@ Toggle window state back
+        -- Toggle Xmobar
+		, ("<Pause>", sendMessage ToggleStruts)
+		-- Kill focused window
+		, ("M-<Backspace>", kill1)
+		-- Fullscreen Mode
+        , ("M-f", sendMessage $ Toggle NBFULL)
+        -- Dynamic Projects
+        , ("M-w", switchProjectPrompt myPromptTheme)
+    	, ("M-S-w", shiftToProjectPrompt myPromptTheme)
+        -- XMonad Prompt
+        -- , ("M-x", xmonadPrompt def)
+        -- Layout Management
+        , ("M-<Tab>", sendMessage NextLayout)
+        ]
+
+myFont = "xft:Iosevka Nerd Font Mono:style=Italic:size=9:antialias=true:hinting=true,xft:Deja Vu:size=9"
+
+myPromptTheme = def
+    { font                  = myFont
+    , bgColor               = "#c0c0c0"
+    , fgColor               = "#2f4f4f"
+    , fgHLight              = "#000000"
+    , bgHLight              = "#ffffff"
+    , borderColor           = "#808080"
+    , promptBorderWidth     = 0
+    , height                = 20
+    , position              = Top
+    }
