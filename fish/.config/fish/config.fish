3ef13c7c0373b4855688d55096899daab49aa2ed
dbeley <6568955+dbeley@users.noreply.github.com>
2019-11-23 11:29:47 +0100

    Update Firefox, fish, mpd.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -19,6 +19,8 @@ alias emc "emacsclient -nw"
 alias enw "emacs -nw"
 alias merde "fuck"
 
+alias cpr "rsync -azvhP --size-only --inplace --stats"
+
 set FZF_DEFAULT_COMMAND "fd --type file --ignore-case --hidden --follow --exclude .git"
 set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set FZF_ALT_C_COMMAND "fd --ignore-case --hidden -t d"
