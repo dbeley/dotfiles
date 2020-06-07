1e7a4ae0a46f9df6b893cca7af2927ef3c4842c6
dbeley <6568955+dbeley@users.noreply.github.com>
2020-06-07 12:29:22 +0200

    Update feh git.

--- Autres/Écrans/SG13-home.sh
+++ Autres/Écrans/SG13-home.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-1 --off --output DP-2 --off --output HDMI-1 --off --output HDMI-2 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal
+xrandr --output DP-1 --off --output DP-2 --off --output HDMI-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal --output HDMI-2 --off --output DVI-D-1 --mode 1920x1080 --pos 1920x0 --rotate normal
