bb5a70e851958249e4debc415cf2584755f00331
dbeley <6568955+dbeley@users.noreply.github.com>
2021-01-04 20:51:02 +0100

    Update fish.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -29,6 +29,10 @@ alias expaclist "expac --timefmt='%Y-%m-%d %T' '%l\t%n' | sort -h"
 alias mpv720 "mpv --ytdl-format=\"(bestvideo[height<=720]+bestaudio)[ext=webm]/bestvideo[height<=720]+bestaudio/best[height<=720]/bestvideo+bestaudio/best\" "
 alias mpv1080 "mpv --ytdl-format=\"(bestvideo[height<=1080]+bestaudio)[ext=webm]/bestvideo[height<=1080]+bestaudio/best[height<=1080]/bestvideo+bestaudio/best\" "
 
+# docker aliases
+alias dprune "docker system prune -a"
+alias dprunev "docker system prune -a --volumes"
+
 set FZF_DEFAULT_COMMAND "fd --type file --ignore-case --hidden --follow --exclude .git"
 set FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
 set FZF_ALT_C_COMMAND "fd --ignore-case --hidden -t d"
