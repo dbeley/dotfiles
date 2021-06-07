ee7a847cb6812faf4ea5c2197c2f899d34d88a9a
dbeley <6568955+dbeley@users.noreply.github.com>
2021-06-07 17:06:57 +0200

    emacs: fix paths.

--- doom-emacs/.doom.d/config.el
+++ doom-emacs/.doom.d/config.el
@@ -38,7 +38,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/Nextcloud/3. org/")
+(setq org-directory "~/Nextcloud/03_org/")
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
