fee8c2aa909a194e450315a37153288ebaf87eb3
dbeley <6568955+dbeley@users.noreply.github.com>
2021-07-23 01:24:15 +0200

    Update packages.sh.

--- packages.sh
+++ packages.sh
@@ -25,54 +25,43 @@ STOWLIST+=(sway bash_wayland alacritty gammastep wpgtk wal mime_wayland gtk mako
 #STOWLIST+=(x bash dunst redshift mime feh gtk rofi wal wpgtk)
 
 ## X.org: i3
-#I3="i3-gaps polybar picom alacritty"
 #PKGLIST+=(i3-gaps polybar picom alacritty)
 #STOWLIST+=(alacritty i3 polybar picom)
 
 ## X.org: dwm
-#DWM="xorg-xsetroot libxft-bgra"
 #PKGLIST+=(xorg-xsetroot libxft-bgra)
 
 ## X.org: xmonad
-#XMONAD="xmonad xmonad-contrib xmobar dmenu alacritty picom"
 #PKGLIST+=(xmonad xmonad-contrib xmobar dmenu alacritty picom)
 #STOWLIST+=(alacritty picom xmonad xmobar)
 #########################################################
 
 # Sound
-SOUND="pulseaudio pamixer pavucontrol"
 PKGLIST+=(pulseaudio pamixer pavucontrol)
 
 # Font
-FONT="otf-overpass nerd-fonts-iosevka noto-fonts-emoji noto-fonts noto-fonts-cjk"
 PKGLIST+=(otf-overpass nerd-fonts-iosevka noto-fonts-emoji noto-fonts noto-fonts-cjk)
 
 # Utilities
-UTILITIES="udisks2 udiskie ntfs-3g glances neofetch htop nnn ncdu fd ripgrep exa fzf bat p7zip rsync zathura zathura-pdf-poppler gnome-system-monitor expac"
 PKGLIST+=(udisks2 udiskie ntfs-3g glances neofetch htop nnn ncdu fd ripgrep exa fzf bat p7zip rsync zathura zathura-pdf-poppler gnome-system-monitor expac)
 STOWLIST+=(zathura)
 
 # Software
-SOFTWARE="gnome-keyring nextcloud-client emacs firefox firefox-i18n-fr keepassxc libreoffice-fresh libreoffice-fresh-fr"
 PKGLIST+=(gnome-keyring nextcloud-client emacs firefox firefox-i18n-fr keepassxc libreoffice-fresh libreoffice-fresh-fr)
 STOWLIST+=(doom-emacs)
 
 # Media
-MEDIA="mpv"
 PKGLIST+=(mpv)
 STOWLIST+=(mpv)
 
 # Music
-MUSIC="mpd mpc ncmpcpp mpdscrobble-git"
 PKGLIST+=(mpd mpc ncmpcpp mpdscrobble-git)
 STOWLIST+=(mpd ncmpcpp)
 
 # Python
-PYTHON="python-pipenv python-pynvim"
 PKGLIST+=(python-pipenv python-pynvim)
 
 # Home
-HOME="nfs-utils"
 PKGLIST+=(nfs-utils)
 
 echo paru -S ${PKGLIST[@]}
