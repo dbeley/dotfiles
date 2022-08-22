ffc50727dc087ebe566049d9a96db7984f759f2f
dbeley <6568955+dbeley@users.noreply.github.com>
2022-08-22 23:34:25 +0200

    Update firefox, kanshi

--- firefox/.mozilla/firefox/profile.default/user.js
+++ firefox/.mozilla/firefox/profile.default/user.js
@@ -54,5 +54,6 @@ user_pref("privacy.resistFingerprinting", true);
 user_pref("privacy.resistFingerprinting.autoDeclineNoUserInputCanvasPrompts", false);
 user_pref("signon.rememberSignons", false);
 user_pref("ui.systemUsesDarkTheme", 1);
+user_pref("browser.download.alwaysOpenPanel", false);
 // user_pref("browser.newtabpage.pinned", "[]");
 // user_pref("browser.newtabpage.activity-stream.improvesearch.topSiteSearchShortcuts.havePinned", "duckduckgo");
